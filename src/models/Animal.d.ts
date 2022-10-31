86df40cadd6fa7f81bf61e4fb3827925dcf2d744
Iury Abreu <iury.abreu@delage.com.br>
2022-10-31 12:02:37 -0300

    Tipagens de Usuario e Animal

---
+++ src/models/Animal.d.ts
@@ -0,0 +1,16 @@
+export type Animal = {
+  name: string;
+  latin_name: string;
+  animal_type: string;
+  active_time: string;
+  length_min: string;
+  length_max: string;
+  weight_min: string;
+  weight_max: string;
+  lifespan: number;
+  habitat: string;
+  diet: string;
+  geo_range: string;
+  image_link: string;
+  id: number;
+};
---
+++ src/models/User.d.ts
@@ -0,0 +1,4 @@
+export interface UserType {
+    userName: string
+    avatar: string
+  }
