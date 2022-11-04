bcad7a1bacc66a6dabcf17a352809b4dd45dd074
Iury Abreu <iury.abreu@delage.com.br>
2022-11-04 13:40:49 -0300

    Criando variações de Botao

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,10 +3,14 @@ import { StyledButton } from "./Styles";
 // import './Styles.scss'
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-  
+  secondary?: boolean;
   danger?: boolean;
 };
 
-export default function Button({ danger = false, ...props }: ButtonProps) {
-  return <StyledButton className={`button ${danger ? "danger" : ""}`} {...props} />;
+export default function Button({
+  danger = false,
+  secondary = false,
+  ...props
+}: ButtonProps) {
+  return <StyledButton danger={danger} secondary={secondary} {...props} />;
 }
--- src/components/Button/Styles.ts
+++ src/components/Button/Styles.ts
@@ -1,26 +1,32 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const StyledButton = styled.button`
-
-  background-color: rgba(0, 71, 186, .7);
+export const StyledButton = styled.button<{
+  danger: boolean;
+  secondary: boolean;
+}>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(pros) =>
+    pros.secondary ? "rgb(160, 238, 192)" : "rgb(0,71,186)"};
   border: 0 solid #e5e7eb;
   box-sizing: border-box;
-  color: #fff;
+  color: ${(pros) => (pros.secondary ? "#333" : "#fff")};
   display: flex;
   font-family: ui-sans-serif, system-ui, -apple-system, system-ui, "Segoe UI",
     Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif,
     "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-  font-size: 1rem;
   font-weight: 700;
+  font-size: 1.25rem;
   justify-content: center;
   line-height: 1.75rem;
-  padding: 0.75rem 1.65rem;
-  position: relative;
   text-align: center;
   text-decoration: none #000 solid;
   text-decoration-thickness: auto;
-  width: 30%;
-  max-width: 460px;
+  width: 20vw;
+  height: auto;
+  padding: 0.75rem 3rem;
+  margin-top: 8px;
   position: relative;
   cursor: pointer;
   transform: rotate(-2deg);
@@ -47,8 +53,7 @@ export const StyledButton = styled.button`
     left: 2px;
   }
 
-  @media (min-width: 768px) {
-    padding: 0.75rem 3rem;
-    font-size: 1.25rem;
+  @media (max-width: 600px) {
+    width: 40vw;
   }
 `;
